67cab9d338dd85df16393044ae522db4632b6c19
Szymon <Szymon@DESKTOP-KD7PK0F>
2024-12-05 14:35:49 +0100

    #zej6

--- foo.hpp
+++ foo.hpp
@@ -11,6 +11,15 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > retval(people.size());
 
     // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human a) { a.birthday(); });
+
+    auto it = retval.rbegin();
+
+
+    std::for_each(people.begin(), people.end(), [&](Human a) {
+        *it = a.isMonster() ? 'n' : 'y';
+        ++it;
+    });
 
     return retval;
 }
